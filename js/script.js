609a472c8535188ee6bdce2ffcafe87b6b77fce1
Henrique Luiz <heenluy@gmail.com>
2023-07-06 19:28:57 -0300

    the layout has been finished

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
-console.log("Tudo OK!")
+const ctx = document.getElementById("chart");
+
+new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+});
